10f625d3a5d3eb960b5a38ebd485cb111a69790b
Szymon Mikler <sjmikler@gmail.com>
2021-02-08 14:01:58 +0100

    .

--- experimental/minimize_inner_states.py
+++ experimental/minimize_inner_states.py
@@ -6,8 +6,9 @@ import matplotlib.scale
 
 ds = datasets.cifar10(repeat_train=False, shuffle_train=False)
 model = models.VGG((32, 32, 3), n_classes=10, version=19)
-# model.load_weights('data/VGG19_IMP03_ticket/130735/0.h5')
-model.load_weights('temp/new_trune_workspace_continuous4.h5')
+model.load_weights('data/VGG19_IMP03_ticket/130735/0.h5')
+# model.load_weights('temp/new_trune_workspace_continuous4.h5')
+# model.load_weights('data/VGG13_full/701455/0.h5')
 # model.load_weights('data/VGG19_IMP03_ticket/770423/0.h5')
 # model.load_weights('data/VGG19_IMP03_ticket/770423/1.h5')
 # model.load_weights('data/VGG19_IMP03_ticket/770423/5.h5')
@@ -15,12 +16,12 @@ loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 # %%
 
-method = 'truning'
+method = 'magnitude'
 
 sp2acc = []
 sp = [
-    # *np.arange(0.0, 0.6, 0.2),
-    # *np.arange(0.6, 0.9, 0.02),
+    *np.arange(0.0, 0.6, 0.2),
+    *np.arange(0.6, 0.9, 0.02),
     *np.arange(0.9, 0.99, 0.01),
     *np.arange(0.99, 0.999, 0.001)
 ]
@@ -43,14 +44,10 @@ for sparsity in sp:
                                            saliences=saliences,
                                            config=utils.ddict({'sparsity': sparsity}),
                                            silent=False)
-    # model = pruning.prune_SNIP(model,
-    #                            dataset=ds['train'],
-    #                            config=utils.ddict({'sparsity': sparsity,
-    #                                                'n_batches': 10}))
     pruning.apply_pruning_for_model(model)
 
     Acc = tf.keras.metrics.Mean()
-    for x, y in ds.train:
+    for x, y in ds.valid:
         loss, acc = toolkit.valid_step(x, y, model, loss_fn, training=True)
         Acc(tf.reduce_mean(acc))
     acc = Acc.result()
@@ -81,13 +78,13 @@ ax.set_yscale('function', functions=(forward, inverse))
 fig.show()
 
 ax.set_ylim(0.5, 1.01)
-ax.set_xlabel('1-shot pruning sparsity (~88.2% sparsity start)')
+ax.set_xlabel('1-shot pruning sparsity')
 ax.set_title("Accuracy without retraining the network")
 
-ax.plot(*zip(*sp2acc), linewidth=2, label='Truning Tr (var. comp.)')
+ax.plot(*zip(*sp2acc), linewidth=2, label='Magnitude pruning (VGG19)')
 ax.legend()
 
-ax.set_xlim(0.9, 0.995)
+ax.set_xlim(0.5, 0.995)
 ax.set_xscale('linear')
 fig.show()
 
