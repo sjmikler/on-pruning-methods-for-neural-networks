8dde9c7c79d4085e1b5fc2fbb5f78961e8cc6678
Szymon Mikler <sjmikler@gmail.com>
2021-04-20 00:27:08 +0200

    pruning is a 1-file module now

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,15 +2,12 @@ import tools.datasets as datasets
 import tools.layers as layers
 import tools.models as models
 import tools.parser as parser
-import tools.pruning as pruning
 import tools.utils as utils
 
 __all__ = [
     'datasets',
-    'layers.py',
+    'layers',
     'models',
     'parser',
-    'pruning',
-    'pruning_toolkit.py',
     'utils'
 ]
