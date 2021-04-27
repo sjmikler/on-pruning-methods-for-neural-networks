304f5a71659dabccbdee467c81773ccba177833e
Szymon Mikler <sjmikler@gmail.com>
2021-04-27 02:03:56 +0200

    improved collecting logs

--- collect_logs.py
+++ collect_logs.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import yaml
+from tools.utils import get_date_from_exp
 
 
 def recursive_collect_logs(path):
@@ -28,10 +29,22 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dest', type=str, default=None,
                         help='where to save gathered yaml logs, by default it is the '
                              'same as --path')
+    parser.add_argument('-s', '--sort-by', type=str, default='TIME',
+                        help='which key from the dictionary to use to sort entries')
+    parser.add_argument('--reverse', action="store_false")
     args = parser.parse_args()
     if args.dest is None:
         args.dest = os.path.join(args.path, 'gathered_logs.yaml')
 
     logs = recursive_collect_logs(args.path)
+
+    if args.sort_by.lower() == 'time':
+        logs = sorted(logs, key=lambda x: get_date_from_exp(x), reverse=parser.reverse)
+    else:
+        key = args.sort_by
+        logs = sorted(logs, key=lambda x: x[key], reverse=parser.reverse)
+
     with open(args.dest, 'w') as f:
         yaml.safe_dump_all(logs, f)
+
+datetime.datetime.strptime('2021.03.12 5:3', "%Y.%m.%d %H:%M")
