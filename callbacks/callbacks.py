e3c500450ddf00bcacd48e48aa23dc81e4138988
Szymon Mikler <sjmikler@gmail.com>
2021-05-28 00:46:14 +0200

    New callback

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -20,3 +20,26 @@ class CosinePruningCallback(tf.keras.callbacks.Callback):
                                            config={"sparsity": 1 - density},
                                            silent=silent)
             pruning_utils.apply_pruning_for_model(model)
+
+
+class PolynomialPruningCallback(tf.keras.callbacks.Callback):
+    def __init__(self, decay_steps, alpha, interval=100, verbose_interval=2000):
+        super().__init__()
+        self.schedule = tf.keras.optimizers.schedules.PolynomialDecay(1.0,
+                                                                      decay_steps,
+                                                                      end_learning_rate=alpha,
+                                                                      power=3.0)
+        self.step = 0
+        self.interval = interval
+        self.verbose_interval = verbose_interval
+
+    def on_train_batch_begin(self, batch, logs=None):
+        if batch % self.interval == 0:
+            self.step += self.interval
+            density = self.schedule(self.step)
+
+            silent = (self.step % self.verbose_interval) != 0
+            model = pruning_utils.prune_l1(model=self.model,
+                                           config={"sparsity": 1 - density},
+                                           silent=silent)
+            pruning_utils.apply_pruning_for_model(model)
