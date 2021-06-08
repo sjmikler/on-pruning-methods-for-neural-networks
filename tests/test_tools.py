822bb5f021e60786745c365142aba56879aafea5
Szymon Mikler <sjmikler@gmail.com>
2021-06-08 16:54:01 +0200

    Update tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -3,14 +3,14 @@ from unittest import TestCase
 
 class TestExperiment(TestCase):
     def test_todict(self):
-        from tools.utils import Experiment
-        exp = Experiment({})
+        from tools import LazyExperiment
+        exp = LazyExperiment({})
         d = exp.todict()
         self.assertEqual(len(d), 0)
         self.assertIsInstance(d, dict)
 
     def test_deepcopy(self):
         from copy import deepcopy
-        from tools.utils import Experiment
-        exp = Experiment({"abc": {"cde": 1, "eef": 2}, "uyu": 14, 2: True})
+        from tools import LazyExperiment
+        exp = LazyExperiment({"abc": {"cde": 1, "eef": 2}, "uyu": 14, 2: True})
         exp = deepcopy(exp)
