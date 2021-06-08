829c5e61f4e3a5962202e9472b74fdf1c115ef14
Szymon Mikler <sjmikler@gmail.com>
2021-06-08 02:01:41 +0200

    Resnet shortcut mode A

--- modules/tf_helper/models.py
+++ modules/tf_helper/models.py
@@ -24,65 +24,74 @@ class GemPool(tf.keras.layers.Layer):
         flow = tf.pow(flow, self.p)
 
         if self.pool:
-            flow = tf.keras.layers.AvgPool2D(self.pool, padding='same',
-                                             dtype='float32')(flow)
+            flow = tf.keras.layers.AvgPool2D(
+                self.pool, padding="same", dtype="float32"
+            )(flow)
         else:
-            flow = tf.keras.layers.GlobalAvgPool2D(dtype='float32')(flow)
+            flow = tf.keras.layers.GlobalAvgPool2D(dtype="float32")(flow)
 
-        flow = tf.pow(flow, tf.divide(1., self.p))
+        flow = tf.pow(flow, tf.divide(1.0, self.p))
         return tf.cast(flow, input_dtype)
 
     def build(self, *args):
-        self.p = tf.Variable(self.initial_value, trainable=True, name='gempool_p',
-                             dtype='float32')
+        self.p = tf.Variable(
+            self.initial_value, trainable=True, name="gempool_p", dtype="float32"
+        )
         super().build(*args)
 
 
-def classifier(flow,
-               n_classes,
-               regularizer=None,
-               bias_regularizer=None,
-               initializer='glorot_uniform',
-               pooling='avgpool',
-               ):
-    if pooling == 'catpool':
+def classifier(
+    flow,
+    n_classes,
+    regularizer=None,
+    bias_regularizer=None,
+    initializer="glorot_uniform",
+    pooling="avgpool",
+):
+    if pooling == "catpool":
         maxp = tf.keras.layers.GlobalMaxPool2D()(flow)
         avgp = tf.keras.layers.GlobalAvgPool2D()(flow)
         flow = tf.keras.layers.Concatenate()([maxp, avgp])
-    if pooling == 'avgpool':
+    if pooling == "avgpool":
         flow = tf.keras.layers.GlobalAvgPool2D()(flow)
-    if pooling == 'maxpool':
+    if pooling == "maxpool":
         flow = tf.keras.layers.GlobalMaxPool2D()(flow)
-    if pooling == 'gempool':
+    if pooling == "gempool":
         flow = GemPool(initial_value=3.0)(flow)
 
     # multiple-head version
     if isinstance(n_classes, Iterable):
         outs = [
-            tf.keras.layers.Dense(n_class,
-                                  bias_regularizer=bias_regularizer,
-                                  kernel_regularizer=regularizer,
-                                  kernel_initializer=initializer)(flow) for n_class in
-            n_classes
+            tf.keras.layers.Dense(
+                n_class,
+                bias_regularizer=bias_regularizer,
+                kernel_regularizer=regularizer,
+                kernel_initializer=initializer,
+            )(flow)
+            for n_class in n_classes
         ]
     else:
-        outs = tf.keras.layers.Dense(n_classes,
-                                     bias_regularizer=bias_regularizer,
-                                     kernel_regularizer=regularizer,
-                                     kernel_initializer=initializer)(flow)
+        outs = tf.keras.layers.Dense(
+            n_classes,
+            bias_regularizer=bias_regularizer,
+            kernel_regularizer=regularizer,
+            kernel_initializer=initializer,
+        )(flow)
     return outs
 
 
-def VGG(input_shape,
-        n_classes,
-        version=None,
-        l1_reg=0,
-        l2_reg=1e-4,
-        group_sizes=(1, 1, 2, 2, 2),
-        features=(64, 128, 256, 512, 512),
-        pools=(2, 2, 2, 2, 2),
-        regularize_bias=True,
-        **kwds):
+def VGG(
+    input_shape,
+    n_classes,
+    version=None,
+    l1_reg=0,
+    l2_reg=1e-4,
+    group_sizes=(1, 1, 2, 2, 2),
+    features=(64, 128, 256, 512, 512),
+    pools=(2, 2, 2, 2, 2),
+    regularize_bias=True,
+    **kwds,
+):
     print(f"VGG: unknown parameters: {list(kwds)}")
     if version:
         if version == 11:
@@ -96,8 +105,9 @@ def VGG(input_shape,
         else:
             raise KeyError(f"Unkown version={version}!")
 
-    regularizer = tf.keras.regularizers.l1_l2(l1_reg,
-                                              l2_reg) if l2_reg or l1_reg else None
+    regularizer = (
+        tf.keras.regularizers.l1_l2(l1_reg, l2_reg) if l2_reg or l1_reg else None
+    )
     bias_regularizer = regularizer if regularize_bias else None
 
     def conv3x3(*args, **kwds):
@@ -132,10 +142,9 @@ def VGG(input_shape,
             flow = conv3x3(filters=width)(flow)
             flow = bn_relu(flow)
 
-    outs = classifier(flow,
-                      n_classes,
-                      regularizer=regularizer,
-                      bias_regularizer=bias_regularizer)
+    outs = classifier(
+        flow, n_classes, regularizer=regularizer, bias_regularizer=bias_regularizer
+    )
     model = tf.keras.Model(inputs=inputs, outputs=outs)
     return model
 
@@ -150,50 +159,63 @@ def ResNet(
     strides=(1, 2, 2),
     group_sizes=(2, 2, 2),
     features=(16, 32, 64),
-    initializer='he_uniform',
-    activation='tf.nn.relu',
-    final_pooling='avgpool',
+    initializer="he_uniform",
+    activation="tf.nn.relu",
+    final_pooling="avgpool",
     dropout=0,
     preactivate_blocks=True,
     regularize_bias=True,
-    head=(
-            ('conv', 16, 3, 1),
-    ),
-    **kwds
+    shortcut_mode_a=False,
+    head=(("conv", 16, 3, 1),),
+    **kwds,
 ):
     if version:
         raise KeyError("Versions not defined yet!")
     print(f"ResNet: unknown parameters: {list(kwds)}")
 
     activation_func = eval(activation)
-    regularizer = tf.keras.regularizers.l1_l2(l1_reg,
-                                              l2_reg) if l2_reg or l1_reg else None
+    regularizer = (
+        tf.keras.regularizers.l1_l2(l1_reg, l2_reg) if l2_reg or l1_reg else None
+    )
     bias_regularizer = regularizer if regularize_bias else None
 
     def conv(filters, kernel_size, use_bias=False, **kwds):
-        return tf.keras.layers.Conv2D(filters,
-                                      kernel_size,
-                                      padding='same',
-                                      use_bias=use_bias,
-                                      kernel_initializer=initializer,
-                                      kernel_regularizer=regularizer,
-                                      bias_regularizer=bias_regularizer,
-                                      **kwds)
+        return tf.keras.layers.Conv2D(
+            filters,
+            kernel_size,
+            padding="same",
+            use_bias=use_bias,
+            kernel_initializer=initializer,
+            kernel_regularizer=regularizer,
+            bias_regularizer=bias_regularizer,
+            **kwds,
+        )
 
     def shortcut(x, filters, strides):
         if x.shape[-1] != filters or strides != 1:
-            return tf.keras.layers.Conv2D(filters,
-                                          kernel_size=1,
-                                          use_bias=False,
-                                          strides=strides,
-                                          kernel_initializer=initializer,
-                                          kernel_regularizer=regularizer)(x)
+            if shortcut_mode_a:
+                m_filters = filters - x.shape[-1]
+                m_width = x.shape[1] // strides
+                m_height = x.shape[2] // strides
+                return tf.pad(
+                    x[:, :m_width, :m_height], [[0, 0], [0, 0], [0, 0], [m_filters, 0]]
+                )
+            else:
+                return tf.keras.layers.Conv2D(
+                    filters,
+                    kernel_size=1,
+                    use_bias=False,
+                    strides=strides,
+                    kernel_initializer=initializer,
+                    kernel_regularizer=regularizer,
+                )(x)
         else:
             return x
 
     def bn_relu(x, remove_relu=False):
-        x = tf.keras.layers.BatchNormalization(beta_regularizer=bias_regularizer,
-                                               gamma_regularizer=bias_regularizer)(x)
+        x = tf.keras.layers.BatchNormalization(
+            beta_regularizer=bias_regularizer, gamma_regularizer=bias_regularizer
+        )(x)
         return x if remove_relu else activation_func(x)
 
     def simple_block(flow, filters, strides, preactivate):
@@ -227,14 +249,14 @@ def ResNet(
 
     # BUILDING HEAD OF THE NETWORK
     for name, *args in head:
-        if name == 'conv':
-            bias = True if 'bias' in args else False
+        if name == "conv":
+            bias = True if "bias" in args else False
             flow = conv(args[0], args[1], strides=args[2], use_bias=bias)(flow)
-        if name == 'maxpool':
+        if name == "maxpool":
             flow = tf.keras.layers.MaxPool2D(args[0])(flow)
-        if name == 'avgpool':
+        if name == "avgpool":
             flow = tf.keras.layers.AvgPool2D(args[0])(flow)
-        if name == 'relu':
+        if name == "relu":
             flow = tf.nn.relu(flow)
 
     # BUILD THE RESIDUAL BLOCKS
@@ -251,12 +273,14 @@ def ResNet(
     flow = bn_relu(flow, remove_relu=True)
     flow = tf.nn.relu(flow)  # use relu here even if different activation is choosen
 
-    outputs = classifier(flow,
-                         n_classes,
-                         regularizer=regularizer,
-                         bias_regularizer=bias_regularizer,
-                         initializer=initializer,
-                         pooling=final_pooling)
+    outputs = classifier(
+        flow,
+        n_classes,
+        regularizer=regularizer,
+        bias_regularizer=bias_regularizer,
+        initializer=initializer,
+        pooling=final_pooling,
+    )
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     return model
 
@@ -282,71 +306,79 @@ def WRN(N, K, *args, **kwds) -> tf.keras.Model:
     """
     assert (N - 4) % 6 == 0
     size = int((N - 4) / 6)
-    return ResNet(*args, group_sizes=(size, size, size),
-                  features=(16 * K, 32 * K, 64 * K), **kwds)
+    return ResNet(
+        *args, group_sizes=(size, size, size), features=(16 * K, 32 * K, 64 * K), **kwds
+    )
 
 
-def LeNet(input_shape,
-          n_classes,
-          l1_reg=0,
-          l2_reg=0,
-          layer_sizes=(300, 100),
-          initializer='glorot_uniform',
-          **kwds):
+def LeNet(
+    input_shape,
+    n_classes,
+    l1_reg=0,
+    l2_reg=0,
+    layer_sizes=(300, 100),
+    initializer="glorot_uniform",
+    **kwds,
+):
     print(f"LeNet: unknown parameters: {list(kwds)}")
-    regularizer = tf.keras.regularizers.l1_l2(l1_reg,
-                                              l2_reg) if l2_reg or l1_reg else None
+    regularizer = (
+        tf.keras.regularizers.l1_l2(l1_reg, l2_reg) if l2_reg or l1_reg else None
+    )
     initializer = initializer
 
     def dense(*args, **kwds):
-        return tf.keras.layers.Dense(*args,
-                                     **kwds,
-                                     kernel_initializer=initializer,
-                                     kernel_regularizer=regularizer)
+        return tf.keras.layers.Dense(
+            *args,
+            **kwds,
+            kernel_initializer=initializer,
+            kernel_regularizer=regularizer,
+        )
 
     inputs = tf.keras.layers.Input(shape=input_shape)
 
     flow = tf.keras.layers.Flatten()(inputs)
     for layer_size in layer_sizes:
-        flow = dense(layer_size, activation='relu')(flow)
+        flow = dense(layer_size, activation="relu")(flow)
 
     outs = dense(n_classes, activation=None)(flow)
     model = tf.keras.Model(inputs=inputs, outputs=outs)
     return model
 
 
-def LeNetConv(input_shape,
-              n_classes,
-              l1_reg=0,
-              l2_reg=0,
-              initializer='glorot_uniform',
-              **kwds):
+def LeNetConv(
+    input_shape, n_classes, l1_reg=0, l2_reg=0, initializer="glorot_uniform", **kwds
+):
     print(f"Unknown parameters: {list(kwds)}")
-    regularizer = tf.keras.regularizers.l1_l2(l1_reg,
-                                              l2_reg) if l2_reg or l1_reg else None
+    regularizer = (
+        tf.keras.regularizers.l1_l2(l1_reg, l2_reg) if l2_reg or l1_reg else None
+    )
     initializer = initializer
 
     def dense(*args, **kwds):
-        return tf.keras.layers.Dense(*args,
-                                     **kwds,
-                                     kernel_initializer=initializer,
-                                     kernel_regularizer=regularizer)
+        return tf.keras.layers.Dense(
+            *args,
+            **kwds,
+            kernel_initializer=initializer,
+            kernel_regularizer=regularizer,
+        )
 
     def conv(*args, **kwds):
-        return tf.keras.layers.Conv2D(*args,
-                                      **kwds,
-                                      kernel_initializer=initializer,
-                                      kernel_regularizer=regularizer)
+        return tf.keras.layers.Conv2D(
+            *args,
+            **kwds,
+            kernel_initializer=initializer,
+            kernel_regularizer=regularizer,
+        )
 
     inputs = tf.keras.layers.Input(shape=input_shape)
 
-    flow = conv(20, 5, activation='relu')(inputs)
+    flow = conv(20, 5, activation="relu")(inputs)
     flow = tf.keras.layers.MaxPool2D(2)(flow)
-    flow = conv(50, 5, activation='relu')(flow)
+    flow = conv(50, 5, activation="relu")(flow)
     flow = tf.keras.layers.MaxPool2D(2)(flow)
 
     flow = tf.keras.layers.Flatten()(flow)
-    flow = dense(500, activation='relu')(flow)
+    flow = dense(500, activation="relu")(flow)
 
     outs = dense(n_classes, activation=None)(flow)
     model = tf.keras.Model(inputs=inputs, outputs=outs)
@@ -361,7 +393,7 @@ def get_model_from_alias(alias, input_shape, n_classes):
         return WRN(int(N), int(K), input_shape=input_shape, n_classes=n_classes)
 
     elif m := re.match(r"VGG(\d+)", alias):
-        N, = m.groups()
+        (N,) = m.groups()
         return VGG(input_shape=input_shape, n_classes=n_classes, version=int(N))
 
     else:
